1ddbce0d08371f920189888fe8c61c44645a21be
Nazar Potipaka <mutado.nzr@gmail.com>
2020-11-30 17:45:48 +0200

    Update authentication controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
 use Log;
+use Session;
 use \App\Http\Requests\LoginRequest;
-
+use Carbon\Carbon;
 
 class LoginController extends Controller
 {
@@ -16,24 +17,51 @@ class LoginController extends Controller
         return view('pages.auth.login');
     }
 
-    public function login(LoginRequest $requestFields)
+    public function login(Request $request, LoginRequest $requestFields)
     {
-        // $validator = $this->validate($request, [
-        //     'email'=>'required|string|email',
-        //     'password'=>'required|string',
-        // ]);
-
         $attributes = $requestFields->only(['email', 'password']);
-        // dd($attributes);
-        if (Auth::attempt($attributes,true)) {
-            return redirect()->route('hub.main');
+        Log::info($requestFields->password);
+        if ($request->expectsJson()) {
+            return $this->apiLogin($request, $attributes);
+        } else {
+            if (Auth::attempt($attributes, true)) {
+                return redirect()->route('hub.main');
+            }
+            else{
+                return redirect()->back()->withErrors(['result'=>'You entered wrong login or password'])->withInput();
+            }
         }
 
-        return redirect()->back()->withErrors(['result'=>'You entered wrong login or password'])->withInput();
+        Log::error('Unhandled error at login request '.$attributes);
+    }
+
+    private function apiLogin($request, $attributes)
+    {
+        if (Auth::attempt($attributes)) {
+            $user = $request->user();
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+            $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->save();
+
+            Log::info('json');
+            return response()->json([
+                    'token_type'=>'Bearer', 
+                    'access_token' => $tokenResult->accessToken,
+                    'user'=>$user,
+                    'expires_at' => Carbon::parse(
+                        $tokenResult->token->expires_at
+                    )->toDateTimeString()
+                ]);
+        }
+        else{
+            return response()->json(['error'=>'invalid login or password'], 401);
+        }
     }
 
     public function logout()
     {
+        $request->user()->token()->revoke();
         Session::flush();
         Auth::logout();
         return back();
