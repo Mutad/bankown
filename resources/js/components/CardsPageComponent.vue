2652ea0579ea411356522327acc012b294d78ce3
Nazar Potipaka <mutado.nzr@gmail.com>
2021-01-03 15:54:01 +0200

    Create Page for displaying all cards

---
+++ resources/js/components/CardsPageComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex flex-column justify-content-start">
+    <div
+      v-for="card in cards"
+      v-bind:key="card.id"
+      class="d-flex align-items-start plate"
+    >
+      <router-link
+        :to="{ name: 'card', params: { id: card.id } }"
+        class="btn p-0"
+      >
+        <card-plate-component :card="card" />
+      </router-link>
+      <div class="plate margin">
+        Balance: {{ card.balance }} <b>{{ card.currency }}</b> <br />
+        Type: <b>{{ card.type }}</b>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import CardPlateComponent from "./CardPlateComponent.vue";
+export default {
+  components: { CardPlateComponent },
+  computed: {
+    ...mapGetters(["cards"]),
+  },
+};
+</script>
+
+<style>
+</style>
