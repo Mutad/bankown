0a2f6fd813d70f6d5e8090d939120ae8b4227bc6
Nazar Potipaka <mutado.nzr@gmail.com>
2021-01-03 15:54:33 +0200

    Display transactions on card pages

--- resources/js/components/hub/CardComponent.vue
+++ resources/js/components/hub/CardComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="status" class="plate-wrapper">
+  <div v-if="this.card() !== undefined" class="plate-wrapper">
     <div class="plate margin border">
       <h2>This is your card</h2>
       <h6 class="text-muted">Here you can change some settings.</h6>
@@ -23,24 +23,69 @@
       </div>
     </div>
     <hr />
-    <div class="m-4">
+    <div class="m-4" v-if="this.transactions">
       <h2>Transactions</h2>
+      <div
+        class="plate my-2 border"
+        v-for="transaction in this.transactions"
+        v-bind:key="transaction.id"
+      >
+        <div class="d-flex justify-content-between">
+          <h5>Transfer of personal funds</h5>
+          <div class="text-muted">{{ transaction.created_at }}</div>
+        </div>
+        <span v-if="card().id == transaction.sender_card_id">-</span
+        >{{ transaction.amount }} {{ card().currency }}
+      </div>
+    </div>
+  </div>
+  <div v-else class="plate-wrapper">
+    <div class="plate margin border text-center">
+      <h2>Card not found</h2>
+      <div>
+        Go back to
+        <router-link :to="{ name: 'hub' }">Hub</router-link>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import { mapGetters, mapState } from "vuex";
 export default {
+  data() {
+    return {
+      transactions: undefined,
+    };
+  },
   methods: {
     card() {
       return this.cards.filter((card) => card.id == this.$route.params.id)[0];
     },
+    getTransactions() {
+      axios({
+        url: "/api/cards/" + this.card().id + "/transactions",
+        method: "GET",
+      })
+        .then((result) => {
+          this.transactions = result.data;
+          this.transactions.forEach((transaction) => {
+            transaction.created_at = new Date(
+              transaction.created_at
+            ).toLocaleString();
+          });
+        })
+        .catch((err) => {});
+    },
   },
   computed: {
     ...mapGetters(["cards"]),
+    ...mapState(["user"]),
     status() {
       return this.$store.state.cardsStatus === "success";
     },
+  },
+  mounted() {
+    this.getTransactions();
   },
 };
 </script>
